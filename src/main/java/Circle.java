6388eacf5e2d8b541c167fdfea7457c7a9516a7f
Heying778 <yingyinghe2022@gmail.com>
2022-08-16 14:56:06 +0100

    Cant resolve symbol, google said due to 'Maven problem'.

---
+++ src/main/java/Circle.java
@@ -0,0 +1,30 @@
+public class Circle {
+        private double radius;
+
+        public double getRadius(){
+            return radius;
+        }
+
+        double circumstance;
+
+        public Circle() {
+            radius = 0.0;
+        }
+
+        public Circle(double radius) {
+            radius = 0.0;
+        }
+
+        public void setCircle(double radius){
+            this.radius = radius;
+        }
+
+        public void setRadius(double radius){
+            this.radius = radius;
+        }
+        public static double getCircle(){
+            return Math.PI * radius* 2;
+        }
+
+    }
+
---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle raidus = new Circle(5);
+
+
+        System.out.println(radius.getCircumstance());
+
+    }
+
+
+}
